2d6f78d9e25ccae35609703ced9ff4abf0bc403a
jlian9 <47437832+jlian9@users.noreply.github.com>
2019-07-28 14:11:22 -0700

    Update BandRasterCalculatorExample.py

--- BandRasterCalculatorExample.py
+++ BandRasterCalculatorExample.py
@@ -1,3 +1,4 @@
+##import library
 import arcpy
 from arcpy import env
 from arcpy.sa import *
@@ -6,20 +7,14 @@ import os
 arcpy.CheckOutExtension("Spatial")
 arcpy.env.workspace = r"E:\VARI"
 
+##generate a file list end with 'update.tif' from target folder
+###all bands rasters are stored in same folder and the only difference part from file name is "b01"|'b02'|'b03'..etc 
 fileList = [f for f in os.listdir(r'E:\VARI') if f.endswith('update.tif')]
 
-#print(varilist)
-
-#flist=['myd09a1.006_sur_refl_b01_doy2016361_aid0001.tif','myd09a1.006_sur_refl_b01_doy2017001_aid0001.tif','myd09a1.006_sur_refl_b01_doy2017009_aid0001.tif',
-#       'myd09a1.006_sur_refl_b03_doy2016361_aid0001.tif','myd09a1.006_sur_refl_b03_doy2017001_aid0001.tif','myd09a1.006_sur_refl_b03_doy2017009_aid0001.tif',
-#       'myd09a1.006_sur_refl_b04_doy2016361_aid0001.tif','myd09a1.006_sur_refl_b04_doy2017001_aid0001.tif','myd09a1.006_sur_refl_b04_doy2017009_aid0001.tif']
-
-##find unique id 1
-print(fileList[0][23])
 
 
 ###use raster b01 as the indicator and then excute the corespond b03 and b04
-
+##crate list variable to store path name for each band
 band1 = [ ]
 band1 = [ ]
 band3 = [ ]
@@ -28,29 +23,29 @@ band6 = [ ]
 
 
 
-
+##loop the all file list
 for b1 in fileList:
+    ##if this is band 1 file, append it to 'band1' list 
     if b1[23] == "1":
         band1.append(b1)
-        #b6=b1.replace('b02','b06')
+        
+        ##replace the 'b01' by other bands number'b03' and 'b04' to get the file path name for band3 and band4
         b3=b1.replace('b01','b03')
         b4=b1.replace('b01','b04')
+        ##append path name to 'band3' list
         band3.append(b3)
-        #band6.append(b6)
         band4.append(b4)
 
-#print(band1)
-#print('=======================\n')
-#print(band3)
-#print('=======================\n')
-#print(band4)
+##get the file number of band1 
 filenum = len(band1)
-print(filenum)
 
+##loop the file base on the file number in the list 
 for i in range(filenum):
     print('now is working on: '+ band1[i])
     try:
+        ##excute the [i] order of the file in the list and do the raster calculation
         outVIG= (Raster(band4[i])-Raster(band1[i]))/(Raster(band4[i])+Raster(band1[i])-Raster(band3[i]))
+        ##save the output and rename 
         outVIG.save('myd09a1.006_sur_refl_doy'+band1[i][28:35]+'VARI.tif')
         print('myd09a1.006_sur_refl_doy'+band1[i][28:35]+'_VARI.tif is done!')
     except:
