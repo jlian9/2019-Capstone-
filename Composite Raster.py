7de6afac0c4712687ea9323775cda3845bf27f72
jlian9 <47437832+jlian9@users.noreply.github.com>
2019-07-28 14:29:09 -0700

    Update Composite Raster.py

--- Composite Raster.py
+++ Composite Raster.py
@@ -8,6 +8,7 @@ arcpy.CheckOutExtension("Spatial")
 
 
 def comp8(workpath):
+    ##create list variable to store the file path name needed to be composited 
     Day1 = [ ]
     Day2 = [ ]
     Day3 = [ ] 
@@ -16,12 +17,17 @@ def comp8(workpath):
     Day6 = [ ] 
     Day7 = [ ]
     Day8 = [ ]  
+    ##generate a file name list from workpath
     File_list = [f for f in os.listdir(workpath) if f.endswith('.tif')]
+    ##create a interator for name list 
     file = iter(File_list)
     arcpy.env.workspace = workpath
+    ##loop through the iterator
     for i in file:
+            ##filter the file already used 
             if i != Day2 and i != Day3 and i != Day4 and i != Day5 and i != Day6 and i != Day7 and i != Day8:
               try: 
+                ##excute 8 successive files 
                 Day1=i
                 Day2=next(file)
                 Day3=next(file)
@@ -30,8 +36,10 @@ def comp8(workpath):
                 Day6=next(file)
                 Day7=next(file)
                 Day8=next(file)
+                ##composite 8 days file and save, rename
                 outFile= (Raster(Day1)+Raster(Day2)+Raster(Day3)+Raster(Day4)+Raster(Day5)+Raster(Day6)+Raster(Day7)+Raster(Day8))/8
                 outFile.save('PRISM_tmean_stable_4kmD1_'+i[25:32]+'_8day.tif')
                 print('PRISM_tmean_stable_4kmD1_'+i[25:32]+'_8day.tif'+ ' is done!')
               except: 
+                ##if dont have 8 successive files, break the loop
                 break 
