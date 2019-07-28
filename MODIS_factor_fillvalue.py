ece8f865481fdf2cf516292238dc4999ee430924
jlian9 <47437832+jlian9@users.noreply.github.com>
2019-07-28 14:33:39 -0700

    Update MODIS_factor_fillvalue.py

--- MODIS_factor_fillvalue.py
+++ MODIS_factor_fillvalue.py
@@ -9,9 +9,13 @@ arcpy.CheckOutExtension("Spatial")
 arcpy.env.workspace = r"D:\Fire data\2000-2018Raster\SR Band12346\band4"
 flist = [f for f in os.listdir(r'D:\Fire data\2000-2018Raster\SR Band12346\band4') if f.endswith('.tif')]
 
+
+##set the scale factor of this index
 inConstant = 0.0001
 for i in flist:
     print('working on: '+i)
+    ##only excute the data value fall within the valid range
     inRaster = ExtractByAttributes(i,'VALUE>= -100 and VALUE<=16000' ) 
+    ##calculate the raster by multiply the factor
     outTimes = Times(inRaster,inConstant)
     outTimes.save('MYD09A1.006_sur_refl_b04_doy'+i[28:35]+'_aid0001_update.tif')
